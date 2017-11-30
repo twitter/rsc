b55c0893e15c14f5f503d4627d81ecaec848e8d0
Eugene Burmako <eburmako@twitter.com>
2017-11-29 21:06:35 -0800

    Establish three benchmark suites: all, ci and quick
    $ bin/bench
    usage: ~/Projects/rsc/bin/bench <suite>
    
    Supported bench suites:
      * All: reproduces docs/performance.md
      * CI: Rsc-specific subset of All that is run for every commit in CI
      * Quick: Lightweight subset of CI that should be used for sanity checks

--- bench/rsc/jvm/src/main/scala/rsc/bench/RscNativeSchedule.scala
+++ bench/rsc/jvm/src/main/scala/rsc/bench/RscNativeSchedule.scala
@@ -2,13 +2,25 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE.md).
 package rsc.bench
 
-object RscNativeSchedule extends CliBench {
+trait RscNativeSchedule extends CliBench {
   def main(args: Array[String]): Unit = {
     val Array(out) = args
     val bs = new FileFixtures {}
     val fs = bs.re2sRscFiles.map(_.toString)
-    val command = List(out, "-Ystop-after:schedule") ++ fs
+    run(List(out, "-Ystop-after:schedule") ++ fs)
+  }
+
+  def run(command: List[String]): Unit
+}
+
+object ColdRscNativeSchedule extends RscNativeSchedule {
+  def run(command: List[String]): Unit = {
     run("ColdRscNativeSchedule", command, runs = 100, iters = 1)
+  }
+}
+
+object HotRscNativeSchedule extends RscNativeSchedule {
+  def run(command: List[String]): Unit = {
     run("HotRscNativeSchedule", command, runs = 1, iters = 100)
   }
 }
