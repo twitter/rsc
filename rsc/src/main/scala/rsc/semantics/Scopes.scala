8d5c3d30cbdc314e13c719d7c9801a83fddc63ba
Eugene Burmako <eburmako@twitter.com>
2018-03-23 17:06:29 -0700

    Turn off spaces.inImportCurlyBraces
    None of our Scalameta projects use this setting, which unnecessarily
    complicates sharing code snippets between the projects.

--- rsc/src/main/scala/rsc/semantics/Scopes.scala
+++ rsc/src/main/scala/rsc/semantics/Scopes.scala
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE.md).
 package rsc.semantics
 
-import java.util.{ HashMap, Map }
+import java.util.{HashMap, Map}
 import scala.collection.mutable
 import rsc.pretty._
 import rsc.syntax._
