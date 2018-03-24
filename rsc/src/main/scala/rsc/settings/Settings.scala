4da69de35cb86c6f8674e110981ea84e4fd8d1d0
Eugene Burmako <eburmako@twitter.com>
2018-03-23 17:06:32 -0700

    Add -cp as an alias for -classpath

--- rsc/src/main/scala/rsc/settings/Settings.scala
+++ rsc/src/main/scala/rsc/settings/Settings.scala
@@ -21,7 +21,7 @@ object Settings {
       args match {
         case "--" +: rest =>
           loop(settings, false, args)
-        case "-classpath" +: s_cp +: rest if allowOptions =>
+        case ("-classpath" | "-cp") +: s_cp +: rest if allowOptions =>
           val cp = s_cp.split(File.pathSeparator).map(s => Paths.get(s)).toList
           loop(settings.copy(classpath = settings.classpath ++ cp), true, rest)
         case opt +: rest if allowOptions && opt.startsWith("-Xprint:") =>
