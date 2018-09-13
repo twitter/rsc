90923a663b46a9955bdc93688d0b039903f45b8a
Eugene Burmako <eburmako@twitter.com>
2018-09-13 10:15:19 -0700

    Upgrade to Scalafix 0.6.0-RC1

--- scalafix/rules/src/main/scala/scalafix/internal/v0/SemanticdbRule.scala
+++ scalafix/rules/src/main/scala/scalafix/internal/v0/SemanticdbRule.scala
@@ -8,7 +8,7 @@ abstract class SemanticdbRule(legacyIndex: SemanticdbIndex, name: String)
     extends SemanticRule(legacyIndex, name) {
 
   lazy val index: DocumentIndex = {
-    val scalafixDoc = legacyIndex.asInstanceOf[DocSemanticdbIndex].doc
+    val scalafixDoc = legacyIndex.asInstanceOf[LegacySemanticdbIndex].doc
     val semanticdbDoc = scalafixDoc.internal.textDocument
     new DocumentIndex(semanticdbDoc)
   }
