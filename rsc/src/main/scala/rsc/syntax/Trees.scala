f81dc06706b6b79df7c1a165efb74e8ac7cbd0bd
Eugene Burmako <eburmako@twitter.com>
2018-10-24 14:09:15 -0700

    Reformat code

--- rsc/src/main/scala/rsc/syntax/Trees.scala
+++ rsc/src/main/scala/rsc/syntax/Trees.scala
@@ -72,9 +72,7 @@ final case class DefnClass(
   def paramss = Nil
 }
 
-final case class DefnConstant(mods: Mods, id: TermId)
-    extends Stat
-    with TermOutline
+final case class DefnConstant(mods: Mods, id: TermId) extends Stat with TermOutline
 
 final case class DefnCtor(mods: Mods, id: CtorId, paramss: List[List[Param]], rhs: Term)
     extends DefnDef
