296ab06cb0dd64d907e6c993fcf9ebb325b134c2
Eugene Burmako <eburmako@twitter.com>
2018-11-20 19:56:42 -0800

    Remove dead code

--- scalafix/rules/src/main/scala/rsc/rules/pretty/SemanticdbPrinter.scala
+++ scalafix/rules/src/main/scala/rsc/rules/pretty/SemanticdbPrinter.scala
@@ -8,7 +8,6 @@ import rsc.pretty._
 import rsc.rules._
 import rsc.rules.semantics._
 import scala.collection.mutable
-import scala.meta._
 import scala.meta.internal.{semanticdb => s}
 import scala.meta.internal.semanticdb.Scala._
 import scala.meta.internal.semanticdb.Scala.{Descriptor => d}
@@ -16,71 +15,6 @@ import scala.meta.internal.semanticdb.Scala.{Names => n}
 import scalafix.internal.v0._
 
 class SemanticdbPrinter(env: Env, index: DocumentIndex, config: RscCompatConfig) extends Printer {
-  def pprint(tree: s.Tree): Unit = tree match {
-    case s.OriginalTree(range) =>
-      str(index.substring(range).get)
-    case s.ApplyTree(fn, args) =>
-      pprint(fn)
-      rep("(", args, ", ", ")")(pprint)
-    case s.TypeApplyTree(fn, targs) =>
-      pprint(fn)
-      rep("[", targs, ", ", "]")(pprint)
-    case s.SelectTree(qual, id) =>
-      // FIXME: https://github.com/twitter/rsc/issues/142
-      val needsParens = qual match {
-        case s.OriginalTree(range) =>
-          val originalTerm = index.substring(range).get.parse[Term].get
-          originalTerm match {
-            case _: Term.ApplyInfix => true
-            case _ => false
-          }
-        case _ => false
-      }
-      if (needsParens) str("(")
-      pprint(qual)
-      if (needsParens) str(")")
-      str(".")
-      pprint(id.get.symbol)
-    case s.IdTree(sym) =>
-      sym.owner.desc match {
-        case d.None =>
-          pprint(sym)
-        case _: d.Package | _: d.Term =>
-          pprint(s.SelectTree(s.IdTree(sym.owner), Some(s.IdTree(sym))))
-        case d.Type(name) =>
-          if (env.lookupThis(name) == sym.owner) {
-            pprint(sym.owner)
-            str(".this.")
-            pprint(sym)
-          } else {
-            // TODO: This looks incorrect.
-            // str(".")
-            ???
-          }
-        case desc => sys.error(s"unsupported desc $desc")
-      }
-    case s.FunctionTree(params, term) =>
-      str("{")
-      params match {
-        case Seq() => str("() => ")
-        case Seq(id) =>
-          pprint(id.symbol)
-          str(" => ")
-        case _ =>
-          rep("(", params, ", ", ") => ")(id => pprint(id.symbol))
-      }
-      pprint(term)
-      str("}")
-    case s.MacroExpansionTree(expandee, _) =>
-      expandee match {
-        case s.ApplyTree(s.IdTree("scala/reflect/package.materializeClassTag()."), Nil) =>
-          str("_root_.scala.reflect.`package`.classTag")
-        case _ =>
-          pprint(expandee)
-      }
-    case _ => sys.error(s"unsupported tree $tree")
-  }
-
   def pprint(tpe: s.Type): Unit = {
     def prefix(tpe: s.Type): Unit = {
       tpe match {
--- scalafix/rules/src/main/scala/scalafix/internal/v0/DocumentIndex.scala
+++ scalafix/rules/src/main/scala/scalafix/internal/v0/DocumentIndex.scala
@@ -2,39 +2,8 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE.md).
 package scalafix.internal.v0
 
-import scala.meta._
-import scala.meta.internal.{semanticdb => s}
-
 case class DocumentIndex(legacyIndex: LegacySemanticdbIndex) {
-  lazy val doc: s.TextDocument = {
-    legacyIndex.doc.internal.textDocument
-  }
-
-  lazy val input: Input = {
-    Input.VirtualFile(doc.uri, doc.text)
-  }
-
-  def substring(range: Option[s.Range]): Option[String] = {
-    range.flatMap { range =>
-      if (doc.text.nonEmpty) {
-        val pos = Position.Range(
-          input,
-          range.startLine,
-          range.startCharacter,
-          range.endLine,
-          range.endCharacter)
-        Some(pos.text)
-      } else {
-        None
-      }
-    }
-  }
-
   lazy val symbols: DocumentSymbols = {
     DocumentSymbols(legacyIndex)
-  }
-
-  lazy val synthetics: Map[s.Range, s.Synthetic] = {
-    doc.synthetics.map(synth => synth.range.get -> synth).toMap
   }
 }
