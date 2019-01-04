af952a4c2d9edbe079495ee121ebd368b4dc8f54
Win Wang <winw@twitter.com>
2019-01-03 16:10:24 -0800

    Add Init to rewrite targets (fix #230)

--- scalafix/input/src/main/scala/rsc/tests/BetterRscCompat.scala
+++ scalafix/input/src/main/scala/rsc/tests/BetterRscCompat.scala
@@ -9,4 +9,28 @@ import scala.collection.mutable
 object BetterRscCompat_Test {
   def x1 = 42
   def x2 = mutable.Map(1 -> "1")
+
+  class CA[A]
+
+  class CB[A](x: A)
+
+  class CC[A](x: Int)
+
+  class CD extends CA
+
+  class CE extends CB
+
+  class CF extends CC(1)
+
+  class CG extends CB(42)
+
+  class CH extends CB[Int](43)
+
+  class CI extends CG
+
+  object nested {
+    class CA3[A, B, C](x: A, y: B, z: C)
+  }
+
+  class CB3[B](b: B) extends nested.CA3(new CA, b, "z")
 }
--- scalafix/output/src/main/scala/rsc/tests/BetterRscCompat.scala
+++ scalafix/output/src/main/scala/rsc/tests/BetterRscCompat.scala
@@ -5,4 +5,28 @@ import scala.collection.mutable
 object BetterRscCompat_Test {
   def x1: Int = 42
   def x2: mutable.Map[Int, String] = mutable.Map(1 -> "1")
+
+  class CA[A]
+
+  class CB[A](x: A)
+
+  class CC[A](x: Int)
+
+  class CD extends CA[Nothing]
+
+  class CE extends CB[Unit]
+
+  class CF extends CC[Nothing](1)
+
+  class CG extends CB[Int](42)
+
+  class CH extends CB[Int](43)
+
+  class CI extends CG
+
+  object nested {
+    class CA3[A, B, C](x: A, y: B, z: C)
+  }
+
+  class CB3[B](b: B) extends nested.CA3[CA[Nothing], B, String](new CA, b, "z")
 }
