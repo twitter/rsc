34f22087e905b79dc44a714796d61a11e7e79c42
Eugene Burmako <eburmako@twitter.com>
2019-01-06 15:21:00 -0800

    Demote Todo._works to private Todo.impl

--- rsc/src/main/scala/rsc/outline/Todo.scala
+++ rsc/src/main/scala/rsc/outline/Todo.scala
@@ -5,14 +5,16 @@ package rsc.outline
 import java.util.{Queue, LinkedList}
 import rsc.pretty._
 import rsc.syntax._
+import scala.collection.JavaConverters._
 
 final class Todo private () extends Pretty {
-  val _works: Queue[(Env, Work)] = new LinkedList[(Env, Work)]
-  def isEmpty: Boolean = _works.isEmpty
-  def add(env: Env, work: Work): Unit = _works.add(env -> work)
+  private val impl: Queue[(Env, Work)] = new LinkedList[(Env, Work)]
+  def isEmpty: Boolean = impl.isEmpty
+  def add(env: Env, work: Work): Unit = impl.add(env -> work)
   def add(env: Env, tpt: Tpt): Unit = add(env, Sketch(tpt))
   def add(env: Env, within: ModWithin): Unit = add(env, Sketch(within))
-  def remove(): (Env, Work) = _works.remove()
+  def remove(): (Env, Work) = impl.remove()
+  def toList: List[(Env, Work)] = impl.asScala.toList
   def printStr(p: Printer): Unit = PrettyTodo.str(p, this)
   def printRepl(p: Printer): Unit = PrettyTodo.repl(p, this)
 }
