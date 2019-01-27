a85351cc23e03667db1291a622462ebb81ea87f3
Eugene Burmako <eburmako@twitter.com>
2019-01-27 10:23:31 -0800

    Cache classpath load misses
                     Before   After
    =================================
    benchParse
    benchIndex
    benchOutline     25816ms  4925ms
    benchSemanticdb  28930ms  6073ms

--- rsc/src/main/scala/rsc/classpath/Classpath.scala
+++ rsc/src/main/scala/rsc/classpath/Classpath.scala
@@ -3,13 +3,30 @@
 package rsc.classpath
 
 import java.nio.file._
+import java.util.Hashtable
 import rsc.semantics._
 import scala.meta.internal.{semanticdb => s}
 import scala.meta.internal.symtab._
 
 final class Classpath private (symtab: SymbolTable) extends AutoCloseable {
+  private sealed trait Status
+  private case object Present extends Status
+  private case object Missing extends Status
+  private val containsCache = new Hashtable[Symbol, Status]
+
   def contains(sym: Symbol): Boolean = {
-    symtab.info(sym).nonEmpty
+    val cached = containsCache.get(sym)
+    cached match {
+      case Present =>
+        true
+      case Missing =>
+        false
+      case null =>
+        val result = symtab.info(sym).nonEmpty
+        if (result) containsCache.put(sym, Present)
+        else containsCache.put(sym, Missing)
+        result
+    }
   }
 
   def apply(sym: Symbol): s.SymbolInformation = {
