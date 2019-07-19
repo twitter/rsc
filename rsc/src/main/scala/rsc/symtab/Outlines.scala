53c28ea4e0cbaa25ab26be5a25e42bd547b014f7
Win Wang <winw@twitter.com>
2019-07-19 10:05:20 -0700

    Rename private vals for easier debugging

--- rsc/src/main/scala/rsc/symtab/Outlines.scala
+++ rsc/src/main/scala/rsc/symtab/Outlines.scala
@@ -8,39 +8,39 @@ import rsc.syntax._
 import rsc.util._
 
 trait Outlines {
-  private val impl = new LinkedHashMap[Symbol, Outline]
+  private val outlinesImpl = new LinkedHashMap[Symbol, Outline]
 
   object outlines {
     def apply(sym: Symbol): Outline = {
-      val outline = impl.get(sym)
+      val outline = outlinesImpl.get(sym)
       if (outline == null) crash(sym)
       outline
     }
 
     def contains(sym: Symbol): Boolean = {
-      impl.containsKey(sym)
+      outlinesImpl.containsKey(sym)
     }
 
     def get(sym: Symbol): Option[Outline] = {
-      val outline = impl.get(sym)
+      val outline = outlinesImpl.get(sym)
       if (outline != null) Some(outline)
       else None
     }
 
     def put(sym: Symbol, outline: Outline): Unit = {
-      if (impl.containsKey(sym) && !sym.desc.isPackage) {
-        val existingOutline = impl.get(sym)
+      if (outlinesImpl.containsKey(sym) && !sym.desc.isPackage) {
+        val existingOutline = outlinesImpl.get(sym)
         if (outline == existingOutline) return
         crash(sym)
       }
       sym match {
         case NoSymbol => crash(outline)
-        case other => impl.put(other, outline)
+        case other => outlinesImpl.put(other, outline)
       }
     }
 
     def result: LinkedList[Outline] = {
-      new LinkedList(impl.values)
+      new LinkedList(outlinesImpl.values)
     }
   }
 }
