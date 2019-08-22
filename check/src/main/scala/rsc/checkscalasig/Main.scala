cd26a8a1c67ced8ab1965f9b81557086e72c5276
Win Wang <winw@twitter.com>
2019-08-22 15:47:32 -0700

    Check rsc/nsc output classfiles with checkscalasig

--- check/src/main/scala/rsc/checkscalasig/Main.scala
+++ check/src/main/scala/rsc/checkscalasig/Main.scala
@@ -2,12 +2,17 @@ package rsc.checkscalasig
 
 import java.nio.file.Path
 import _root_.rsc.checkbase.{SimpleBase, ToolResult}
+import rsc.checkscalasig.Settings.ClassFiles
 
 /**
  * Example invocation (in sbt):
  * check/runMain rsc.checkscalasig.Main --classpath $JAVALIB:SCALALIB C.scala
  *
  * Replace $JAVALIB:SCALALIB with the actual paths
+ *
+ * Or in --classfiles mode to check output
+ *
+ * check/runMain rsc.checkscalasig.Main --classfiles rsc_output.jar nsc_output.jar
  */
 object Main extends SimpleBase[Settings, Path, Path] {
 
@@ -16,11 +21,11 @@ object Main extends SimpleBase[Settings, Path, Path] {
   }
 
   def nscResult(settings: Settings): ToolResult[Path] = {
-    nsc(settings.cp, settings.ins)
+    settings.classfiles.nsc.map(Right(_)).getOrElse(nsc(settings.cp, settings.ins))
   }
 
   def rscResult(settings: Settings): ToolResult[Path] = {
-    rsc(settings.cp, settings.ins)
+    settings.classfiles.rsc.map(Right(_)).getOrElse(rsc(settings.cp, settings.ins))
   }
 
   def checker(settings: Settings, nscResult: Path, rscResult: Path): Checker = {
